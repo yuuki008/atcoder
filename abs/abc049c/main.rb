84ede49975c879248b7dfd93c20529d86fb2e046
yuuki008 <s22ah261@gmail.com>
2024-03-20 23:57:41 +0900

    abc049c

--- abs/abc049c/main.rb
+++ abs/abc049c/main.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+S = gets.chomp
+
+while S.length > 0
+  if S.end_with?("dream")
+    S = S[0..-6]
+  elsif S.end_with?("dreamer")
+    S = S[0..-8]
+  elsif S.end_with?("erase")
+    S = S[0..-6]
+  elsif S.end_with?("eraser")
+    S = S[0..-7]
+  else
+    puts "NO"
+    exit
+  end
+end
+
+puts 'YES'
