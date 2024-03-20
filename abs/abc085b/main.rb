19e1f4ecc5b79ea538e3b0447b2938ea536a7745
yuuki008 <s22ah261@gmail.com>
2024-03-20 21:04:03 +0900

    abc085b

--- abs/abc085b/main.rb
+++ abs/abc085b/main.rb
@@ -1,2 +1,12 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+D = N.times.map { gets.chomp.to_i }.sort.reverse
+
+kagami_mochi = [D[0]]
+for d in D[1..-1] do
+  if kagami_mochi[-1] > d
+    kagami_mochi << d
+  end
+end
+
+puts kagami_mochi.size
