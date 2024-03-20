a84bfc67958cce1846c86f5d0108b904475d44d3
yuuki008 <s22ah261@gmail.com>
2024-03-20 20:37:59 +0900

    abc083b

--- abs/abc083b/main.rb
+++ abs/abc083b/main.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N, A, B = gets.chomp.split(" ").map(&:to_i)
+
+sum = 0
+
+for n in 1..N do
+  num = n.to_s.split("").map(&:to_i).sum
+  if num >= A && num <= B
+    sum += n
+  end
+end
+
+puts sum
