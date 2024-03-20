7873f57f60651b7d356fd6a2042861a1b72972a5
yuuki008 <s22ah261@gmail.com>
2024-03-20 20:21:55 +0900

    abc081b

--- abs/abc081b/main.rb
+++ abs/abc081b/main.rb
@@ -1,2 +1,18 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.split("")[0].to_i
+A = gets.chomp.split(" ").map(&:to_i)
+
+count = 0
+
+def all_even?(arr)
+  arr.each { |a| return false if a % 2 != 0 }
+  return true
+end
+
+temp_arr = A
+while all_even?(temp_arr) do
+  temp_arr = temp_arr.map { |a| a / 2 }
+  count += 1
+end
+
+puts count
