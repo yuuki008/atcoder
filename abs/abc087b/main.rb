adf71e91b2c312bb0ce414365b680e5f33404dd0
yuuki008 <s22ah261@gmail.com>
2024-03-20 20:33:58 +0900

    abc087b

--- abs/abc087b/main.rb
+++ abs/abc087b/main.rb
@@ -1,2 +1,27 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+GOHYAKU = gets.chomp.to_i
+HYAKU = gets.chomp.to_i
+GOJUU = gets.chomp.to_i
+X = gets.chomp.to_i
+
+count = 0
+
+def over_x?(num)
+  num > X
+end
+
+for i in 0..GOHYAKU do
+  gohyaku = 500 * i
+  break if over_x?(gohyaku)
+  for j in 0..HYAKU do
+    gohyaku_hyaku = gohyaku + 100 * j
+    break if over_x?(gohyaku_hyaku)
+    for k in 0..GOJUU do
+      gohyaku_hyaku_gojuu = gohyaku_hyaku + 50 * k
+      break if over_x?(gohyaku_hyaku_gojuu)
+      count += 1 if gohyaku_hyaku_gojuu == X
+    end
+  end
+end
+
+puts count
