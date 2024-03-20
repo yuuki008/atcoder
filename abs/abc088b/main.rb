510e038005ce4512f3120616e444a9495cc1450e
yuuki008 <s22ah261@gmail.com>
2024-03-20 20:55:45 +0900

    abc088b

--- abs/abc088b/main.rb
+++ abs/abc088b/main.rb
@@ -1,2 +1,25 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+A = gets.chomp.split.map(&:to_i)
+
+alice = 0
+bob = 0
+
+def alice_turn?(n)
+  n % 2 == 0
+end
+
+for n in 0..N - 1 do
+  max = A.max
+
+  if alice_turn?(n)
+    alice += max
+  else
+    bob += max
+  end
+
+  max_first_index = A.index(max)
+  A.delete_at(max_first_index)
+end
+
+puts alice - bob
