f543738180101d7db12b5dedfe4e96e06de67596
yuuki008 <s22ah261@gmail.com>
2024-03-30 14:11:16 +0900

    feat: b 問題

--- abc346/b/main.rb
+++ abc346/b/main.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+# wbwbwwbwbwbw が永遠に続く文字列で W 個の w と B 個の b を持つ部分文字列が存在しているかどうかを判定する
+keyword = "wbwbwwbwbwbw"
+W, B = gets.chomp.split(" ").map(&:to_i)
+
+S = keyword * 20
+
+keyword.each_char.with_index do |c, i|
+  substring = S[i..i+W+B-1]
+  w_count = substring.count("w")
+  if w_count == W
+    puts "Yes"
+    exit
+  end
+end
+
+puts "No"
