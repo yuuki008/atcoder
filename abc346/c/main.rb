97a6c37246dec4d96ec826efb21ba179043e769b
yuuki008 <s22ah261@gmail.com>
2024-03-30 15:23:27 +0900

    C 問題通ったーー

--- abc346/c/main.rb
+++ abc346/c/main.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
 N, K = gets.chomp.split(" ").map(&:to_i)
 A = gets.chomp.split(" ").map(&:to_i)
-A.uniq!
 
 
-ans = 0
-for i in 1..K
-  ans += i unless A.include?(i) 
+def sum_numbers_up_to(n)
+  return n * (n + 1) / 2
+end
+
+ans = sum_numbers_up_to(K)
+
+for a in A.uniq do
+  next if a > K
+  ans -= a
 end
 
 puts ans
