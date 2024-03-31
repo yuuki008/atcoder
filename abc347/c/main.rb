54248a59f43cc6eb83e7069e8b361c4b690ba9ad
yuuki008 <s22ah261@gmail.com>
2024-03-31 09:07:45 +0900

    C 問題

--- abc347/c/main.rb
+++ abc347/c/main.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N, A, B = gets.chomp.split(" ").map(&:to_i)
+D = gets.chomp.split(" ").map(&:to_i)
+
+day_of_weeks = D.map { |d| d % (A+B) == 0 ? A+B : d % (A+B) }.uniq.sort
+
+if day_of_weeks.max - day_of_weeks.min < A
+  puts "Yes"
+  exit
+end
+
+(1..(day_of_weeks.size-1)).each do |i|
+  if day_of_weeks[i] - day_of_weeks[i-1] > B
+    puts "Yes"
+    exit
+  end
+end
+
+puts "No"
