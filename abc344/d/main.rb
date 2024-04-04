3ff7ad983d5f18e5e3d23201d149562c7b4d251a
yuuki008 <s22ah261@gmail.com>
2024-04-04 16:58:00 +0900

    TLE

--- abc344/d/main.rb
+++ abc344/d/main.rb
@@ -1,2 +1,25 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+T = gets.chomp
+N = gets.chomp.to_i
+AA = (1..N).map { gets.chomp.split[1..-1]}
+
+$hash = {}
+
+def recursive(i, substring, times)
+  times = $hash[substring].nil? ? times : [$hash[substring], times].min
+  $hash[substring] = times
+
+  return if AA[i].nil?
+
+  for a in AA[i]
+    recursive(i+1, substring+a, times+1)
+    recursive(i+1, substring, times)
+  end
+end
+
+for a in AA[0]
+  recursive(1, a, 1)
+end
+
+
+puts $hash[T] || -1
