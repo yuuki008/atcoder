08981d796f7914c1a796c2f7c12b45f9bc95ade8
yuuki008 <s22ah261@gmail.com>
2024-04-06 11:52:19 +0900

    C問題

--- abc343/c/main.rb
+++ abc343/c/main.rb
@@ -1,2 +1,12 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+
+N = gets.chomp.to_i
+
+(1..Math.cbrt(N).to_i).reverse_each do |x|
+  cube = x ** 3
+
+  if cube.to_s == cube.to_s.reverse
+    puts cube
+    exit
+  end
+end
