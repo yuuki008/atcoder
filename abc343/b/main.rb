7a9ee231dab8ea8be73ac7e596fb3f6fd0b9ffd2
yuuki008 <s22ah261@gmail.com>
2024-04-06 11:29:11 +0900

    b 問題

--- abc343/b/main.rb
+++ abc343/b/main.rb
@@ -1,2 +1,27 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+
+ans = []
+N.times do |i|
+  A = gets.chomp.split.map(&:to_i)
+
+  temp = []
+  A.each.with_index do |a, i|
+    if a == 1
+      temp.push(i+1)
+    end
+  end
+
+  next if temp.size == 0
+
+  ans.push(temp)
+end
+
+if ans.size == 0
+  puts ''
+  exit
+end
+
+ans.each do |a|
+  puts a.join(' ')
+end
