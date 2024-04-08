0c32a020f8e3297c0bc8102d27fe8eb4e09161ff
yuuki008 <s22ah261@gmail.com>
2024-04-08 19:40:01 +0900

    c

--- abc348/c/main.rb
+++ abc348/c/main.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+AC = N.times.map { gets.chomp.split.map(&:to_i) }
+
+hash = {}
+
+AC.each do |a, c|
+  if hash[c].nil?
+    hash[c] = a
+  else
+    hash[c] = [hash[c], a].min
+  end
+end
+
+puts hash.values.max || 0
