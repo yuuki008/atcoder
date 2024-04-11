e29c516bdda2ddbd6e79aee3b7a7d3172c0b489a
yuuki008 <s22ah261@gmail.com>
2024-04-11 22:57:04 +0900

    TLE

--- abc342/c/main.rb
+++ abc342/c/main.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+S = gets.chomp
+Q = gets.chomp.to_i
+CD = Q.times.map { gets.chomp.split }
+
+CD.each do |cd|
+  c = cd[0]
+  d = cd[1]
+
+  S.gsub!(c, d)
+end
+
+puts S
