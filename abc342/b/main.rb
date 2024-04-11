aaf6136fafc6335ea3bd90d80853ad6dee55719d
yuuki008 <s22ah261@gmail.com>
2024-04-11 22:50:39 +0900

    b 問題

--- abc342/b/main.rb
+++ abc342/b/main.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+P = gets.chomp.split.map(&:to_i)
+Q = gets.chomp.to_i
+AB = Q.times.map { gets.chomp.split.map(&:to_i) }
+
+AB.each do |ab|
+  min_p_index = [P.index(ab[0]), P.index(ab[1])].min
+  puts P[min_p_index]
+end
