be9a4cc4b16a68a138d2e6feefaa5359a621b641
yuuki008 <s22ah261@gmail.com>
2024-04-12 19:37:01 +0900

    feat: TLE

--- abc342/c/main.rb
+++ abc342/c/main.rb
@@ -1,14 +1,32 @@
 #!/usr/bin/env ruby
 N = gets.chomp.to_i
-S = gets.chomp
+S = gets.chomp.split("")
 Q = gets.chomp.to_i
 CD = Q.times.map { gets.chomp.split }
 
-CD.each do |cd|
-  c = cd[0]
-  d = cd[1]
-
-  S.gsub!(c, d)
+hash = Hash.new([])
+S.each.with_index do |s, i|
+  hash[s] += [i]
 end
 
-puts S
+CD.each do |(c, d)|
+  next if c == d
+
+  if hash[d].nil?
+    hash[d] = hash[c]
+  else
+    hash[d] += hash[c]
+  end
+
+  hash.delete(c)
+end
+
+ans = [""] * N
+
+hash.each do |k, v|
+  v.each do |i|
+    ans[i] = k
+  end
+end
+
+puts ans.join
