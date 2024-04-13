919bfc51ef7cea3b92e9a30604edfeb36a8229bb
yuuki008 <s22ah261@gmail.com>
2024-04-13 21:21:52 +0900

    feat: b 問題

--- abc349/b/main.rb
+++ abc349/b/main.rb
@@ -1,2 +1,23 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+S = gets.chomp.split("")
+
+hash = Hash.new(0)
+
+S.each do |s|
+  hash[s] += 1
+end
+
+
+n_hash = Hash.new([])
+S.uniq.each do |s|
+  n_hash[hash[s]] += [s]
+end
+
+n_hash.each do |k, v|
+  if v.size != 2
+    puts 'No'
+    exit
+  end
+end
+
+puts 'Yes'
