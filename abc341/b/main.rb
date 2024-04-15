f436e139f975943b4a1c4e21729eaa83b6675494
yuuki008 <s22ah261@gmail.com>
2024-04-15 19:46:54 +0900

    feat: b 問題

--- abc341/b/main.rb
+++ abc341/b/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+A = gets.chomp.split.map(&:to_i)
+ST = []
+(N - 1).times do 
+  ST << gets.chomp.split.map(&:to_i)
+end
+
+(N-1).times do |i|
+  divide = A[i] / ST[i][0]
+
+  A[i] -= divide * ST[i][0]
+  A[i+1] += divide * ST[i][1]
+end
+
+puts A[-1]
