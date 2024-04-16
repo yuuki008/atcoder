43bb900d9a6d51d2f37dbb15bc868a403d1cfd7d
yuuki008 <s22ah261@gmail.com>
2024-04-16 21:14:45 +0900

    b

--- abc340/b/main.rb
+++ abc340/b/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+Q = gets.chomp.to_i
+queries = Q.times.map { gets.split.map(&:to_i) }
+
+A = []
+
+ans = []
+queries.each do |query|
+  if query[0] == 1
+    A << query[1]
+  else
+    ans << A[-query[1]]
+  end
+end
+
+puts ans.join("\n")
