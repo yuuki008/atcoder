97b135b81ea04341e8d3a61767b84b737717f152
yuuki008 <s22ah261@gmail.com>
2024-04-20 21:11:51 +0900

    b

--- abc350/b/main.rb
+++ abc350/b/main.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N, Q = gets.chomp.split.map(&:to_i)
+T = gets.chomp.split.map(&:to_i)
+
+tooth = Array.new(N, true)
+
+T.each do |t|
+  if tooth[t-1] == true
+    tooth[t-1] = false
+  else
+    tooth[t-1] = true
+  end
+end
+
+puts tooth.select { |t| t === true }.size
