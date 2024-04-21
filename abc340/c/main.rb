96211ba15058a5d7b0743614724b217bb57195d0
yuuki008 <s22ah261@gmail.com>
2024-04-21 17:19:50 +0900

    c 問題

--- abc340/c/main.rb
+++ abc340/c/main.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.to_i
+
+$hash = Hash.new(nil)
+def f(n)
+  return 0 if (n == 1)
+  return $hash[n] if $hash[n]
+  return $hash[n] = f(n/2) + f((n+1)/2) + n
+end
+
+f(N)
+
+puts $hash[N]
