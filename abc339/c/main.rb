313ac9221f7d6f961d0ee5cceb0016e612a7dc78
yuuki008 <s22ah261@gmail.com>
2024-04-21 18:49:33 +0900

    feat: c 問題

--- abc339/c/main.rb
+++ abc339/c/main.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.to_i
+A = gets.chomp.split.map(&:to_i)
+
+temp = 0
+
+ans = 0
+A.each do |a|
+  temp += a
+
+  if temp < 0
+    ans += temp.abs
+    temp = 0
+  end
+end
+
+puts temp
