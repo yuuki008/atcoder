20d23898dc2ad33f5d6f48e9a05624979de86756
yuuki008 <s22ah261@gmail.com>
2024-04-22 23:27:17 +0900

    c

--- abc338/c/main.rb
+++ abc338/c/main.rb
@@ -1,2 +1,30 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.to_i
+Q = gets.chomp.split.map(&:to_i)
+A = gets.chomp.split.map(&:to_i)
+B = gets.chomp.split.map(&:to_i)
+
+ans = 0
+
+0.upto(1_000_000) do |a|
+  b = 1_000_000
+  f = true
+  N.times do |i|
+    rest = Q[i] - A[i] * a
+    if rest < 0
+      f = false
+      break
+    end
+    next if B[i] == 0
+    tmp = rest / B[i]
+    b = [b, tmp].min
+  end
+
+  if f
+    ans = [a+b, ans].max
+  else
+    break
+  end
+end
+
+puts ans
