024fb06fcc6717d177ae003b69754fea1eca15c0
yuuki008 <s22ah261@gmail.com>
2024-04-22 20:15:47 +0900

    b

--- abc338/b/main.rb
+++ abc338/b/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+S = gets.chomp
+
+
+tmp = [0] * 26
+
+S.each_char do |c|
+  tmp[c.ord - 'a'.ord] += 1
+end
+
+max_i = 0
+tmp.each_with_index do |t, i|
+  max_i = i if t > tmp[max_i]
+end
+
+puts (max_i + 'a'.ord).chr
