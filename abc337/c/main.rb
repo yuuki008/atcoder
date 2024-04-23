9a296868a1f7b262882f729bc9fcfe086f1057d9
yuuki008 <s22ah261@gmail.com>
2024-04-23 21:12:16 +0900

    c

--- abc337/c/main.rb
+++ abc337/c/main.rb
@@ -1,2 +1,24 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+A = gets.chomp.split.map(&:to_i)
+
+
+
+hash2 = {}
+
+A.each_with_index do |a, i|
+  hash2[a] = i+1
+end
+
+ans = []
+
+tmp = -1
+
+loop do
+  break if hash2[tmp].nil?
+  ans << hash2[tmp]
+
+  tmp = hash2[tmp]
+end
+
+puts ans.join(' ')
