5b382cee747f45d08f13229587d2f3dc5ab7a556
yuuki008 <s22ah261@gmail.com>
2024-04-23 20:13:33 +0900

    b

--- abc337/b/main.rb
+++ abc337/b/main.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+S = gets.chomp.split('')
+
+a_c = S.count('A')
+b_c = S.count('B')
+c_c = S.count('C')
+
+"A" * a_c + "B" * b_c + "C" * c_c == S.join('') ? puts("Yes") : puts("No")
