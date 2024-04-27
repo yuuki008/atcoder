e8dac7bd5544c6706c818ffdbfd4647d596ed546
yuuki008 <s22ah261@gmail.com>
2024-04-27 10:00:56 +0900

    b

--- abc334/b/main.rb
+++ abc334/b/main.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+A, M, L, R = gets.chomp.split(" ").map(&:to_i)
+
+# A, M, L, R = [-3, 2, 3, 8]
+
+l = (L-A) % M == 0 ? (L-A)/M-1: (L-A)/M 
+r = (R-A)/M
+
+puts r - l
