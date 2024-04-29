75e1df4073f990e686063f49d3bb5c0f777a1733
yuuki008 <s22ah261@gmail.com>
2024-04-29 11:25:37 +0900

    c

--- abc351/c/main.rb
+++ abc351/c/main.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+A = gets.chomp.split.map(&:to_i)
+
+def main(array)
+  if array[-1] == array[-2] && array.size >= 2
+    array.pop
+    array[-1] += 1
+    main(array)
+  end
+end
+
+c = []
+A.each do |a|
+  c << a
+  main(c)
+end
+
+puts c.size
+
