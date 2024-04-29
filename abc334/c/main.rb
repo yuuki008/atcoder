6105d1b46b0874bb17b47e0e6d3e11e83649c1c6
yuuki008 <s22ah261@gmail.com>
2024-04-29 09:32:58 +0900

    途中まで

--- abc334/c/main.rb
+++ abc334/c/main.rb
@@ -1,2 +1,43 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N, K = gets.chomp.split.map(&:to_i)
+A = gets.chomp.split.map(&:to_i)
+
+all = [2] * (N+1)
+
+A.each do |a|
+  all[a] -= 1
+end
+
+rest = []
+
+all.each_with_index do |a, i|
+  if a == 1
+    rest << i
+  end
+end
+
+
+if rest.length > 2 && rest.length % 2 != 0
+  max_difference = nil
+  max_index = nil
+
+  rest.each_cons(2).with_index do |(a, b), index|
+    difference = (a - b).abs
+    if max_difference.nil? || difference > max_difference
+      max_difference = difference
+      max_index = index
+    end
+  end
+  
+  rest.delete_at(max_index)
+end
+
+ans = 0
+
+if rest.length > 1
+  (0..rest.length-1).step(2) do |i|
+    ans += (rest[i+1] - rest[i]).abs
+  end
+end
+
+puts ans
