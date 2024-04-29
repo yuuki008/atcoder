0c1bddf24583565be9c71b67497c8692054436f5
yuuki008 <s22ah261@gmail.com>
2024-04-29 09:44:07 +0900

    b

--- abc351/b/main.rb
+++ abc351/b/main.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+A = N.times.map { gets.chomp }
+B = N.times.map { gets.chomp }
+
+N.times.each do |i|
+  next if A[i] == B[i]
+
+  N.times.each do |j|
+    if A[i][j] != B[i][j]
+      puts "#{i+1} #{j+1}"
+      exit
+    end
+  end
+end
