64e32c77fbb34e8eba896e65c97c8fde6413b9b9
yuuki008 <s22ah261@gmail.com>
2024-04-30 18:47:04 +0900

    b

--- abc333/b/main.rb
+++ abc333/b/main.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+S1, S2 = gets.chomp.split("")
+T1, T2 = gets.chomp.split("")
+
+nodes = ['A', 'B', 'C', 'D', 'E']
+
+S_distance = (nodes.index(S1) - nodes.index(S2)).abs
+T_distance = (nodes.index(T1) - nodes.index(T2)).abs
+
+S_distance = S_distance === 4 ? 1 : S_distance
+T_distance = T_distance === 4 ? 1 : T_distance
+
+if S_distance == 1 && T_distance == 1
+  puts "Yes"
+elsif S_distance != 1 && T_distance != 1
+  puts "Yes"
+else
+  puts "No"
+end
+
