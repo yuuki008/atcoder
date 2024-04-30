b5ee5af369112e10606563fc23dc01afa62205c8
yuuki008 <s22ah261@gmail.com>
2024-04-30 19:21:13 +0900

    c

--- abc333/c/main.rb
+++ abc333/c/main.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+
+repunit = [1, 11, 111, 1111, 11111, 111111, 1111111, 11111111, 111111111, 1111111111, 11111111111, 111111111111]
+
+tmp = []
+for i in repunit
+  for j in repunit
+    for k in repunit
+      tmp << i + j + k 
+    end
+  end
+end
+
+puts tmp.uniq.sort[N-1]
