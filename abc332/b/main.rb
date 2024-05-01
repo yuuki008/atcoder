2bc5f2ff7a10513a570010b39825e9102dd55114
yuuki008 <s22ah261@gmail.com>
2024-05-01 20:24:09 +0900

    b

--- abc332/b/main.rb
+++ abc332/b/main.rb
@@ -1,2 +1,22 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+K, G, M = gets.split.map(&:to_i)
+
+g = G
+m = M
+
+K.times do
+  if g == G
+    g = 0
+  elsif m == 0
+    m += M
+  elsif G-g < m
+    t = G-g
+    g += t
+    m -= t
+  else
+    g += m
+    m = 0
+  end
+end
+
+puts "#{g} #{m}"
