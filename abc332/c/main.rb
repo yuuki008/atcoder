41bdabe45cd2961a40ce89d36d3bfff730290868
yuuki008 <s22ah261@gmail.com>
2024-05-01 20:34:37 +0900

    c

--- abc332/c/main.rb
+++ abc332/c/main.rb
@@ -1,2 +1,29 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N, M = gets.chomp.split.map(&:to_i)
+S = gets.chomp.split("").map(&:to_i)
+
+# 0は予定なし
+# 1は食事の予定
+# 2は競技プログラミングの予定
+
+ans = 0
+cnt = 0
+cnt_1 = 0
+S.each do |s|
+  if s == 1
+    cnt_1 += 1
+    cnt += 1 if cnt_1 > M
+  end
+
+  if s == 2
+    cnt += 1
+  end
+
+  if s == 0
+    ans = [ans, cnt].max
+    cnt_1 = 0
+    cnt = 0
+  end
+end
+
+puts [ans, cnt].max
