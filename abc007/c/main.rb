52c37bf11182fcdaa705d8e8588c234723954061
yuuki008 <s22ah261@gmail.com>
2024-05-01 10:51:07 +0900

    幅優先探索

---
+++ abc007/c/main.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+# R, C = [7, 8]
+# sx, sy = [2, 2]
+# gx, gy = [4, 5]
+# G = [
+# ['#','#','#','#','#','#','#','#'],
+# ['#','.','.','.','.','.','.','#'],
+# ['#','.','#','#','#','#','#','#'],
+# ['#','.','.','#','.','.','.','#'],
+# ['#','.','.','#','#','.','.','#'],
+# ['#','#','.','.','.','.','.','#'],
+# ['#','#','#','#','#','#','#','#'],
+# ]
+
+R, C = gets.split.map(&:to_i)
+sx, sy = gets.split.map { |i| i.to_i - 1 }
+gx, gy = gets.split.map { |i| i.to_i - 1 }
+G = R.times.map { gets.chomp.chars }
+
+dist = Array.new(R) { Array.new(C) { -1 } }
+
+que = []
+
+dist[sx][sy] = 0
+que << [sx, sy]
+
+while que.size > 0
+  x, y = que.shift
+
+  if x == gx && y == gy
+    puts dist[x][y]
+    exit
+  end
+
+
+  if G[x+1][y] == '.' && dist[x+1][y] == -1
+    que << [x+1, y]
+    dist[x+1][y] = dist[x][y] + 1
+  end
+
+  if G[x-1][y] == '.' && dist[x-1][y] == -1
+    que << [x-1, y] 
+    dist[x-1][y] = dist[x][y] + 1
+  end
+
+  if G[x][y+1] == '.' && dist[x][y+1] == -1
+    que << [x, y+1] 
+    dist[x][y+1] = dist[x][y] + 1
+  end
+
+  if G[x][y-1] == '.' && dist[x][y-1] == -1
+    que << [x, y-1]
+    dist[x][y-1] = dist[x][y] + 1
+  end
+end
+
