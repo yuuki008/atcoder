2a91d8732af978c42f8dec9cfa02f2c58dbe36aa
yuuki008 <s22ah261@gmail.com>
2024-05-04 21:31:06 +0900

    c

--- abc352/c/main.rb
+++ abc352/c/main.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+AB = N.times.map { gets.chomp.split.map(&:to_i) }
+
+
+shoulder_to_head_sizes = AB.map { |a, b| b - a }
+top_giant = shoulder_to_head_sizes.index(shoulder_to_head_sizes.max)
+
+puts AB.sum { |a, b| a } +(AB[top_giant][1] - AB[top_giant][0])
