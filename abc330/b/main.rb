70567c633b0c6a9036ba764003317988f325ff62
yuuki008 <s22ah261@gmail.com>
2024-05-06 10:36:06 +0900

    b

--- abc330/b/main.rb
+++ abc330/b/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N, L, R = gets.split.map(&:to_i)
+A = gets.split.map(&:to_i)
+
+ans = []
+A.each do |a|
+  if a < L
+    ans << L
+  elsif a > R
+    ans << R
+  else
+    ans << a
+  end
+end
+
+puts ans.join(" ")
