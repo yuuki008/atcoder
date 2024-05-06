30d4c2b7667431c6747c8d2cfa2d07efdb90a668
yuuki008 <s22ah261@gmail.com>
2024-05-06 13:01:16 +0900

    c

--- abc330/c/main.rb
+++ abc330/c/main.rb
@@ -1,2 +1,12 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+D = gets.chomp.to_i
+
+ans = Float::INFINITY
+(0..Math.sqrt(D)).each do |x|
+  y = Math.sqrt((D - x**2).abs).to_i
+  ans = [ans, (x**2 + y**2 - D).abs].min
+  y += 1
+  ans = [ans, (x**2 + y**2 - D).abs].min
+end
+
+puts ans
