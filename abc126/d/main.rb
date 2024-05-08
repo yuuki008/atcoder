7cdba07908218d9f1cfcd1f6354aa5cceca1d538
yuuki008 <s22ah261@gmail.com>
2024-05-08 20:16:41 +0900

    d

--- abc126/d/main.rb
+++ abc126/d/main.rb
@@ -1,2 +1,24 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+@edges = Array.new(N) { [] }
+@ans = Array.new(N)
+
+(N - 1).times do
+  u, v, w = gets.chomp.split.map(&:to_i)
+  @edges[u - 1] << [v - 1, w]
+  @edges[v - 1] << [u - 1, w]
+end
+
+def dfs(u, c)
+  @ans[u] = c
+
+  @edges[u].each do |v, w|
+    next if @ans[v]
+
+    w.even? ? dfs(v, c) : dfs(v, 1 - c)
+  end
+end
+
+dfs(0, 0)
+
+puts @ans.join("\n")
