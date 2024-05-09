0962ea38517ae1a7032f848905e8ed7343396d9b
yuuki008 <s22ah261@gmail.com>
2024-05-10 00:02:14 +0900

    b

--- dp/b/main.rb
+++ dp/b/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N, K = gets.chomp.split.map(&:to_i)
+H = gets.chomp.split.map(&:to_i)
+
+dp = Array.new(N, 1<<30)
+
+dp[0] = 0
+
+0.upto(N-1) do |i|
+  K.times do |k|
+    j = k + 1
+    dp[i+j] = [dp[i] + (H[i]-H[i+j]).abs, dp[i+j]].min if i+j < N
+  end
+end
+
+puts dp[N-1]
