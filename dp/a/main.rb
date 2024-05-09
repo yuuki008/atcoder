332aae916700b66bef14de63bc7bd96517c3f094
yuuki008 <s22ah261@gmail.com>
2024-05-09 23:43:52 +0900

    feat: a

--- dp/a/main.rb
+++ dp/a/main.rb
@@ -1,2 +1,26 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+H = gets.chomp.split.map(&:to_i)
+
+dp = Array.new(N, -1)
+dp[0] = 0
+
+def dfs(i, dp)
+  return dp[i] if i == 0
+  return dp[i] if dp[i] != -1
+
+  if i == 1
+    dp[i] = (H[i] - H[i - 1]).abs
+  else
+    fp = dfs(i-1, dp) + (H[i] - H[i-1]).abs
+    sp = dfs(i-2, dp) + (H[i] - H[i-2]).abs
+
+    dp[i] = [fp, sp].min
+  end
+
+  dp[i]
+end
+
+dfs(N-1, dp)
+
+puts dp[N-1]
