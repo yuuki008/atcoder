36b1ce70a928c193556cef1e1fb17b8e34ccbbc8
yuuki008 <s22ah261@gmail.com>
2024-05-11 21:12:43 +0900

    b

--- abc353/b/main.rb
+++ abc353/b/main.rb
@@ -1,2 +1,18 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N, K = gets.chomp.split.map(&:to_i)
+A = gets.chomp.split.map(&:to_i)
+
+
+left = K
+ans = 0
+while A.size > 0
+  if A[-1] > left
+    ans += 1
+    left = K
+  else
+    a = A.pop()
+    left -= a
+  end
+end
+
+puts left === K ? ans : ans + 1
