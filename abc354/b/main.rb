060a70dd2749fd2311b5f0242d638f2e7ff31088
yuuki008 <s22ah261@gmail.com>
2024-05-18 21:13:53 +0900

    b

--- abc354/b/main.rb
+++ abc354/b/main.rb
@@ -1,2 +1,12 @@
 #!/usr/bin/env ruby
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+SC = N.times.map { gets.chomp.split }
+S = SC.map { |s, c| s }
+C = SC.map { |s, c| c.to_i }
+
+total_rate = C.sum
+sorted_names = S.sort
+
+winner_index = total_rate % N
+
+puts sorted_names[winner_index]
